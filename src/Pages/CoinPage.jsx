2833b7c04fd276f083abea706dbbd2eb540e7aa7
sanujasethi06 <sethisanuja1998@gmail.com>
2024-03-05 12:32:31 +0530

    Coin list table data completed

--- src/Pages/CoinPage.jsx
+++ src/Pages/CoinPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SingleCoin } from '../config/apis';
+import { useParams } from 'react-router-dom';
+import { CryptoState } from '../cryptoContext';
+import axios from 'axios';
 
 const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const { currency, symbol } = CryptoState();
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+    setCoin(data);
+  }
+  console.log(coin);
+  useEffect(() => {
+    fetchCoin();
+  },[])
   return (
     <div>
       Coin Page
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner/Banner'
+import CoinListTable from '../components/CoinListTable'
 
 const HomePage = () => {
   return (
     <div>
-      <Banner/>
+          <Banner />
+          <CoinListTable/>
     </div>
   )
 }
